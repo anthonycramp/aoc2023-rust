748045fbda8a66e5ad850ad040d3aaa7bf6036ff
Anthony Cramp <anthony.cramp@gmail.com>
2023-12-09 22:16:47 +1030

    Added Day 02 skeleton

---
+++ day02/README.md
@@ -0,0 +1,3 @@
+# [2023 Day 02](https://adventofcode.com/2023/day/2)
+
+words
---
+++ day02/src/main.rs
@@ -0,0 +1,64 @@
+const DAY_NUMBER: &str = "02";
+const INPUT: &str = include_str!("../../inputs/day02.txt");
+// const INPUT: &str = "";
+
+fn main() {
+    println!("Day {} Part 1: {:?}", DAY_NUMBER, part1(INPUT));
+    println!("Day {} Part 2: {:?}", DAY_NUMBER, part2(INPUT));
+}
+
+// replace return type as required by the problem
+fn part1(input: &str) -> i32 {
+    0
+}
+
+// replace return type as required by the problem
+fn part2(input: &str) -> i32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    const TEST_INPUT: &str = r"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+
+    use super::*;
+    use test_support::test_support::TestCase;
+
+    #[test]
+    fn test_part1() {
+        let test_cases = [
+            TestCase {
+                input: TEST_INPUT,
+                expected: 123,
+            },
+            TestCase {
+                input: "abc",
+                expected: 345,
+            },
+        ];
+        for TestCase { input, expected } in test_cases.iter() {
+            assert_eq!(part1(*input), *expected);
+        }
+    }
+
+    #[test]
+    fn test_part2() {
+        let test_cases = [
+            TestCase {
+                input: TEST_INPUT,
+                expected: 123,
+            },
+            TestCase {
+                input: "abc",
+                expected: 345,
+            },
+        ];
+        for TestCase { input, expected } in test_cases.iter() {
+            assert_eq!(part2(*input), *expected);
+        }
+    }
+}

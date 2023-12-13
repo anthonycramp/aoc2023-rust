c0bdb644975dc73536571cf44fc2bcbb8800afef
Anthony Cramp <anthony.cramp@gmail.com>
2023-12-13 21:30:29 +1030

    GREEN add more tests for previous functionality

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -109,6 +109,21 @@ mod tests {
     fn test_card_score() {
         let card = Card::from("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53");
         assert_eq!(8, card.get_score());
+
+        let card = Card::from("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19");
+        assert_eq!(2, card.get_score());
+
+        let card = Card::from("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1");
+        assert_eq!(2, card.get_score());
+
+        let card = Card::from("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83");
+        assert_eq!(1, card.get_score());
+
+        let card = Card::from("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36");
+        assert_eq!(0, card.get_score());
+
+        let card = Card::from("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11");
+        assert_eq!(0, card.get_score());
     }
 
     #[test]

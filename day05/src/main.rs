0555eda40b161713fc1a015f43605c1d97796073
Anthony Cramp <anthony.cramp@gmail.com>
2023-12-17 21:28:26 +1030

    Add skeleton for Day 05

---
+++ day05/src/main.rs
@@ -0,0 +1,88 @@
+const DAY_NUMBER: &str = "05";
+const INPUT: &str = include_str!("../../inputs/day05.txt");
+// const INPUT: &str = "";
+
+fn main() {
+    println!("Day {} Part 1: {:?}", DAY_NUMBER, part1(INPUT));
+    println!("Day {} Part 2: {:?}", DAY_NUMBER, part2(INPUT));
+}
+
+// replace return type as required by the problem
+fn part1(input: &str) -> i32 {
+    0
+}
+
+// replace return type as required by the problem
+fn part2(input: &str) -> i32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    const TEST_INPUT: &str = r"seeds: 79 14 55 13
+
+    seed-to-soil map:
+    50 98 2
+    52 50 48
+    
+    soil-to-fertilizer map:
+    0 15 37
+    37 52 2
+    39 0 15
+    
+    fertilizer-to-water map:
+    49 53 8
+    0 11 42
+    42 0 7
+    57 7 4
+    
+    water-to-light map:
+    88 18 7
+    18 25 70
+    
+    light-to-temperature map:
+    45 77 23
+    81 45 19
+    68 64 13
+    
+    temperature-to-humidity map:
+    0 69 1
+    1 0 69
+    
+    humidity-to-location map:
+    60 56 37
+    56 93 4";
+
+    use super::*;
+    use test_support::test_support::TestCase;
+
+    #[test]
+    #[ignore = "not yet implemented"]
+    fn test_part1() {
+        let test_cases = [TestCase {
+            input: TEST_INPUT,
+            expected: 35,
+        }];
+        for TestCase { input, expected } in test_cases.iter() {
+            assert_eq!(part1(*input), *expected);
+        }
+    }
+
+    #[test]
+    #[ignore = "not yet implemented"]
+    fn test_part2() {
+        let test_cases = [
+            TestCase {
+                input: TEST_INPUT,
+                expected: 123,
+            },
+            TestCase {
+                input: "abc",
+                expected: 345,
+            },
+        ];
+        for TestCase { input, expected } in test_cases.iter() {
+            assert_eq!(part2(*input), *expected);
+        }
+    }
+}

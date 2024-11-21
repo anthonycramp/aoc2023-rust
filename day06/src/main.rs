51054bf98d7ca3a131461e4a9d0cb621fbef067b
Anthony Cramp <anthony.cramp@gmail.com>
2024-11-21 22:55:58 +1030

    Complete Day06 Part 1

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -10,7 +10,38 @@ fn main() {
 
 // replace return type as required by the problem
 fn part1(input: &str) -> i32 {
-    0
+    let mut input_lines = input.lines();
+    let race_times: Vec<_> = input_lines
+        .next()
+        .unwrap()
+        .split_ascii_whitespace()
+        .skip(1)
+        .map(|t| t.parse::<i32>().unwrap())
+        .collect();
+
+    let record_distances: Vec<_> = input_lines
+        .next()
+        .unwrap()
+        .split_ascii_whitespace()
+        .skip(1)
+        .map(|d| d.parse::<i32>().unwrap())
+        .collect();
+
+    (0..race_times.len())
+        .map(|race_index| {
+            compute_record_breaking_combinations_for_race(
+                race_times[race_index],
+                record_distances[race_index],
+            )
+        })
+        .product::<i32>()
+}
+
+fn compute_record_breaking_combinations_for_race(race_time: i32, record_distance: i32) -> i32 {
+    (0..=race_time)
+        .map(|charge_time| charge_time * (race_time - charge_time))
+        .filter(|race_distance| *race_distance > record_distance)
+        .count() as i32
 }
 
 // replace return type as required by the problem
@@ -26,18 +57,11 @@ Distance:  9  40  200"#;
     use test_support::test_support::TestCase;
 
     #[test]
-    #[ignore = "not yet implemented"]
     fn test_part1() {
-        let test_cases = [
-            TestCase {
-                input: TEST_INPUT,
-                expected: 123,
-            },
-            TestCase {
-                input: "abc",
-                expected: 345,
-            },
-        ];
+        let test_cases = [TestCase {
+            input: TEST_INPUT,
+            expected: 288,
+        }];
         for TestCase { input, expected } in test_cases.iter() {
             assert_eq!(part1(*input), *expected);
         }

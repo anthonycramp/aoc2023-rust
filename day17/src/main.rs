cb17277109946d948ff5d94de97b67b5698b393f
Anthony Cramp <anthony.cramp@gmail.com>
2024-12-01 10:43:09 +1030

    Find a path subject to constraints but it is not shortest

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,4 +1,7 @@
-use std::collections::{BinaryHeap, HashMap};
+use std::{
+    collections::{BinaryHeap, HashMap},
+    fmt::Display,
+};
 
 const DAY_NUMBER: &str = "17";
 const INPUT: &str = include_str!("../../inputs/day17.txt");
@@ -102,6 +105,36 @@ impl DesertIslandMap {
 
         neighbours
     }
+
+    fn draw(&self, paths: &Vec<i32>, start: &Location, goal: &Location) {
+        let start_index = self.get_index(start);
+        let goal_index = self.get_index(goal);
+
+        let mut directions = vec![AbsoluteDirection::NONE; self.get_map_size()];
+        let mut current_index = goal_index;
+        while current_index != start_index {
+            let prev_index = paths[current_index] as usize;
+            let current_location = self.get_location(current_index);
+            let prev_location = self.get_location(prev_index);
+            let direction_from_prev_to_current =
+                AbsoluteDirection::compute_direction(&prev_location, &current_location);
+            directions[current_index] = direction_from_prev_to_current;
+            current_index = prev_index;
+        }
+
+        for row in 0..self.map_rows {
+            for col in 0..self.map_cols {
+                let location = Location { row, col };
+                let index = self.get_index(&location);
+                print!(
+                    "{}{} ",
+                    self.heat_loss_map[index],
+                    directions[index].get_display_char()
+                );
+            }
+            println!();
+        }
+    }
 }
 
 #[derive(Clone, PartialEq)]
@@ -125,12 +158,57 @@ impl Location {
     }
 }
 
-#[derive(PartialEq, Eq, Hash)]
+#[derive(PartialEq, Eq, Hash, Clone, Copy)]
 enum AbsoluteDirection {
     NORTH,
     EAST,
     SOUTH,
     WEST,
+    NONE,
+}
+
+impl AbsoluteDirection {
+    fn compute_direction(from: &Location, to: &Location) -> Self {
+        if from.row != to.row && from.col != to.col {
+            Self::NONE
+        } else if from.row == to.row && from.col == to.col {
+            Self::NONE
+        } else if from.row == to.row {
+            if from.col < to.col {
+                Self::EAST
+            } else {
+                Self::WEST
+            }
+        } else if from.col == to.col {
+            if from.row < to.row {
+                Self::SOUTH
+            } else {
+                Self::NORTH
+            }
+        } else {
+            Self::NONE
+        }
+    }
+
+    fn get_opposite_direction(&self) -> AbsoluteDirection {
+        match self {
+            AbsoluteDirection::EAST => AbsoluteDirection::WEST,
+            AbsoluteDirection::WEST => AbsoluteDirection::EAST,
+            AbsoluteDirection::SOUTH => AbsoluteDirection::NORTH,
+            AbsoluteDirection::NORTH => AbsoluteDirection::SOUTH,
+            _ => AbsoluteDirection::NONE,
+        }
+    }
+
+    fn get_display_char(&self) -> char {
+        match self {
+            AbsoluteDirection::EAST => '>',
+            AbsoluteDirection::WEST => '<',
+            AbsoluteDirection::NORTH => '^',
+            AbsoluteDirection::SOUTH => 'v',
+            _ => ' ',
+        }
+    }
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
@@ -165,7 +243,7 @@ fn part1(input: &str) -> i32 {
     );
 
     let mut distance = vec![i32::max_value(); desert_island_map.get_map_size()];
-    let mut last = vec![-1; desert_island_map.get_map_size()];
+    let mut last: Vec<i32> = vec![-1; desert_island_map.get_map_size()];
 
     distance[desert_island_map.get_index(&start)] = 0;
 
@@ -176,33 +254,96 @@ fn part1(input: &str) -> i32 {
     });
 
     while let Some(State { cost, index }) = heap.pop() {
-        if index == desert_island_map.get_index(&goal) {
-            println!("{:?}", &last);
-            return distance[index];
-        }
+        // println!("{}: {}", index, cost);
+        // if we've reached our goal, print all the paths found and
+        // return the shortest distance from start to goal
+        // if index == desert_island_map.get_index(&goal) {
+        //     println!("{:?}", &last);
+        //     return distance[index];
+        // }
 
+        // if the cost to reach this location exceeds a cost on
+        // a path we've already explored, ignore this node
         if cost > distance[index] {
             continue;
         }
 
-        let neighbours = desert_island_map.get_neighbours(&desert_island_map.get_location(index));
+        // next step processes each of the neighbours of the current location
+        let all_neighbours =
+            desert_island_map.get_neighbours(&desert_island_map.get_location(index));
 
-        for (_, location) in neighbours {
+        // However, for part 1, we can't move in the same direction
+        // more than three times. We also can't move backwards (although,
+        // this constraint would be picked up in the shortest path
+        // calculation). Thus, we need to check the recent direction
+        // of the shortest path to the current node and check whether
+        // each neighbour would exceed the forward or reverse progress
+        // constraints, and prune those neighbours if so (doesn't
+        // apply to the start location)
+        let mut direction_to_previous_location = AbsoluteDirection::NONE;
+        let mut three_previous_directions = AbsoluteDirection::NONE;
+
+        if index != desert_island_map.get_index(&start) {
+            let current_location = desert_island_map.get_location(index);
+            let prev_index = last[index];
+            let prev_location = desert_island_map.get_location(prev_index as usize);
+            direction_to_previous_location =
+                AbsoluteDirection::compute_direction(&current_location, &prev_location);
+            let prev_prev_index = last[prev_index as usize];
+            if prev_prev_index >= 0 {
+                let prev_prev_prev_index = last[prev_prev_index as usize];
+                if prev_prev_prev_index >= 0 {
+                    let first_location =
+                        desert_island_map.get_location(prev_prev_prev_index as usize);
+                    let second_location = desert_island_map.get_location(prev_prev_index as usize);
+                    let first_direction =
+                        AbsoluteDirection::compute_direction(&first_location, &second_location);
+                    let third_location = prev_location;
+                    let second_direction =
+                        AbsoluteDirection::compute_direction(&second_location, &third_location);
+                    let fourth_location = current_location;
+                    let third_direction =
+                        AbsoluteDirection::compute_direction(&third_location, &fourth_location);
+                    if first_direction == second_direction && second_direction == third_direction {
+                        three_previous_directions = first_direction;
+                    }
+                }
+            }
+        }
+
+        let mut neighbours = vec![];
+        for (direction, location) in all_neighbours {
+            if direction != direction_to_previous_location && direction != three_previous_directions
+            {
+                neighbours.push(location);
+            }
+        }
+
+        // for each of the valid neighbours
+        // check if they'll be on the shortest path and update
+        for location in neighbours {
             let next = State {
                 cost: cost + desert_island_map.get_heat_loss(&location),
                 index: desert_island_map.get_index(&location),
             };
 
+            // println!("\tChecking {:?}", &next);
+            // println!(
+            //     "\tCurrent distance to {}: {}",
+            //     next.index, distance[next.index]
+            // );
             if next.cost < distance[next.index] {
                 heap.push(next);
                 distance[next.index] = next.cost;
                 last[next.index] = index as i32;
+                // println!("\tAdding {:?}", &next);
             }
         }
     }
 
-    println!("{:?}", &last);
-    0
+    // println!("{:?}", &last);
+    desert_island_map.draw(&last, &start, &goal);
+    distance[desert_island_map.get_index(&goal)]
 }
 
 // replace return type as required by the problem
